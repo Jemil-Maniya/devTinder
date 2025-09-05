2f9e53bbf7337b5d6d2fb2f2a087035c6186853b
Jemil <jemilmaniya27@gmail.com>
2025-09-05 18:37:18 +0530

    use-of0next-function

--- src/app.js
+++ src/app.js
@@ -6,14 +6,25 @@ const app = express();
 //   res.send("NAMASTE DEVLOPERS");
 // });
 
-app.get("/user/:userID/:lastname", (req, res) => {
-  console.log(req.query)
-  console.log(req.params);
-  res.send({ name: "Jemil", password: "nothing" });
-});
 
-app.post("/user", (req, res) => {
-  res.send({ name: "nothing to post", password: "no password" });
+
+app.get("/user", (req, res, next) => {
+console.log(" 1st route without")
+next()
+},
+(req, res, next)=>{
+console.log("2nd route without")
+next()
+}, (req, res, next)=>{
+  console.log("3rd rouye without")
+  next()
+}, (req, res, next)=> {
+  console.log("4th rouye without")
+  next()
+},
+(req, res)=> {
+  console.log("5th route with response")
+  res.send("finally get the working route!")
 });
 
 app.listen(7777, () => {
