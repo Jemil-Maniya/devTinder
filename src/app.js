f04faeac1f08de3d18b31327f8711ed5c60bc800
Jemil <jemilmaniya27@gmail.com>
2025-09-06 12:13:11 +0530

    error-handling

--- src/app.js
+++ src/app.js
@@ -19,6 +19,12 @@ app.get("/admin", isAdminAuth, (req, res) => {
   res.status(200).send("YOU ARE A ADMIN");
 });
 
+app.use("/", (err, req, res) => {
+  if (err) {
+    res.send("SOMETHING WENT WRONG INTERNAL ERROR");
+  }
+});
+
 app.listen(7777, () => {
   console.log("SERVER IS LISTENING YOUR REQUEST");
 });
