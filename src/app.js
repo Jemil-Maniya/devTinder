84afc54c2bf99287c0b0ef1fb815b9a04b80979f
Jemil <jemilmaniya27@gmail.com>
2025-09-12 11:54:16 +0530

    get-APIS

--- src/app.js
+++ src/app.js
@@ -9,18 +9,26 @@ const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
 
-app.post("/signUp", async (req, res) => {
-  const user = new User({
-    firstName: "jemil",
-    lastName: "maniya",
-    email: "jemilmaniya@gmail.com",
-    password: "jemil123",
-  });
+// if we want to make the data from the end user then we have to call the express.json in the app.use
+
+app.use(express.json());
+
+app.get("/feed", async (req, res) => {
   try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    console.log("/Feed error");
+  }
+});
+
+app.post("/signUp", async (req, res) => {
+  try {
+    const user = new User(req.body);
     await user.save();
-    res.send("USER ADDED ");
-  } catch (error) {
-    console.log("ERROR USER NOT ADDED");
+    res.send("USER ADDED");
+  } catch (err) {
+    console.log("USER NOT ADDED");
   }
 });
 
